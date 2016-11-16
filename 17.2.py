e7c4eceb00b66fc91c49c2d14975fb159166a3fa
Ben Humphris <bhumphris@comcast.net>
2016-11-15 23:06:24 -0600

    Add files via upload

---
+++ 17.2.py
@@ -0,0 +1,9 @@
+class Point(object):
+
+     def __init__(self, x=0, y=0):
+         self.x = x
+         self.y = y
+         print "The X coordinate is %d" % self.x, "and the Y coordinate is %d" % self.y
+
+points = Point(10,15)
+
